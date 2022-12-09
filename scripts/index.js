7302f1f158b7b8e8fb6ecb52f938a5c8e54c0b2b
Ekaterina Yurkova <kniffligHexe@yandex.ru>
2022-12-09 19:26:03 +0800

    "cards_appear+transition_popup"

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,37 @@ let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__container');
 
+const cardElements = document.querySelector('.elements');
+//const templateElement = document.querySelector('#element').content;
+const template = document.querySelector('#element');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 function openPopup() {
 
   popup.classList.add('popup_opened');
@@ -31,5 +62,18 @@ editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
 
+const createElement = (cards) => {
+  const element = template.content.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__image').src = cards.link;
+  element.querySelector('.element__image').alt = cards.name;
+  element.querySelector('.element__text').textContent = cards.name;
+ return element;
+}
+const renderElement = (cards) => {
+  cardElements.append(createElement(cards));
 
+}
 
+initialCards.forEach((initcard) => {
+  renderElement(initcard);
+});
